932b6ea2d9ddd4f13e1279d5667390b8d16d1e5d
RND <58924597+radianceltd@users.noreply.github.com>
2020-07-29 10:29:24 +0800

    Initial Commit

---
+++ WeiboDemo/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  WeiboDemo
+//
+//  Created by RND on 2020/7/29.
+//  Copyright © 2020 RND. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
